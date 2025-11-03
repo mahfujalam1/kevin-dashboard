41082ecb9ae1be6ac6b489b87148ea9923286cb9
mahfujalam1 <mahfujahmad424@gmail.com>
2025-11-03 17:53:18 +0600

    final design push

--- src/utils/CustomButton.jsx
+++ src/utils/CustomButton.jsx
@@ -7,35 +7,33 @@ const CustomButton = ({
   className,
 }) => {
   return (
-      <div
-        className={`${className}  p-0.5 rounded-lg inline-block`}
+    <div className={`${className}  p-0.5 rounded-lg inline-block`}>
+      <Button
+        type="default"
+        htmlType="submit"
+        loading={loading}
+        className="w-full bg-[#000000] px-5 py-2 flex justify-center items-center gap-5  rounded-lg border-none"
+        size="large"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+          backgroundColor: "#000000",
+          color: "#ffff", // Ensure text color stays white
+        }}
+        // Custom hover style to maintain background and text color
+        onMouseEnter={(e) => {
+          e.target.style.backgroundColor = "#000000"; // Maintain the same background color on hover
+          e.target.style.color = "#ffff"; // Maintain white text color on hover
+        }}
+        onMouseLeave={(e) => {
+          e.target.style.backgroundColor = "#000000"; // Maintain the same background color when hover ends
+          e.target.style.color = "#ffff"; // Maintain white text color when hover ends
+        }}
       >
-        <Button
-          type="default"
-          htmlType="submit"
-          loading={loading}
-          className="w-full bg-[#FF8133] px-5 py-2 flex justify-center items-center gap-5  rounded-lg border-none"
-          size="large"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "10px",
-            backgroundColor: "#FF8133",
-            color: "#000000", // Ensure text color stays white
-          }}
-          // Custom hover style to maintain background and text color
-          onMouseEnter={(e) => {
-            e.target.style.backgroundColor = "#FF8133"; // Maintain the same background color on hover
-            e.target.style.color = "#000000"; // Maintain white text color on hover
-          }}
-          onMouseLeave={(e) => {
-            e.target.style.backgroundColor = "#FF8133"; // Maintain the same background color when hover ends
-            e.target.style.color = "#000000"; // Maintain white text color when hover ends
-          }}
-        >
-          {children}
-        </Button>
-      </div>
+        {children}
+      </Button>
+    </div>
   );
 };
 
